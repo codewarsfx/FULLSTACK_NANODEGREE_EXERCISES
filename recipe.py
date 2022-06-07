47fd0afce32833b8b96c69b976bb3ab1484d7988
Innocent Chidera Shammah <chiderainnocent001@gmail.com>
2022-06-07 13:17:41 +0100

    initialized recipe project

---
+++ recipe.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return 'hello world '
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
